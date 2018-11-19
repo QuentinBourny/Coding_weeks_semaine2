54876704c3f338917618b15c5c570c21b654a278
valentinches <45173677+valentinches@users.noreply.github.com>
2018-11-19 16:05:55 +0100

    Add files via upload

---
+++ activité_école.py
@@ -0,0 +1,8 @@
+
+
+def activité_école(id_école , tweets):
+    nombre_de_tweets = 0
+    for tweet in tweets:
+        if tweet.user_id != id_école
+            nombre_de_tweets +=1
+    return nombre_de_tweets
